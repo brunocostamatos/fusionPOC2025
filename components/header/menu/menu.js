885356a01f93aab7c2ea11295caef6f02b67a3ea
unknown <brunomatos@labnet.nce.ufrj.br>
2024-05-02 22:14:40 -0300

    corrigindo funcao de verificacao de tela e retirando import desnecessario da funcao scrollToTop

--- components/header/menu/menu.js
+++ components/header/menu/menu.js
@@ -1,6 +1,6 @@
 // Lista de opcoes menu
 const opcoesMenu = [
-    {title:'Home', link: ""},
+    {title:'Home', link: "/"},
     {title:'Call for Contributions',link: "call-for-contributions"},
     {title:'Submit',link: "submit"},
     {title:'Competition',link: "competition"},
@@ -19,7 +19,7 @@ const opcoesMenu = [
           let button = document.createElement('a');
           button.href= element.link
           button.innerHTML = element.title;
-          if(element.link == pageAtual){
+          if(element.link == pageAtual || pageAtual == "" && element.link == "/"){
               button.classList.add('active');
           }
           opcao.appendChild(button);
