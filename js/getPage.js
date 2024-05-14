bf99ae1c76461d6e0d7211b1e88b58c82f6f7fa5
unknown <brunomatos@labnet.nce.ufrj.br>
2024-05-13 21:35:52 -0300

    testando alteracao na mudanca de title da aba do navegador

--- js/getPage.js
+++ js/getPage.js
@@ -7,11 +7,9 @@ export function carregarPagina(url) {
         if(html != '' || html == null || html == undefined){
           const parser = new DOMParser();
           // Converte o HTML em um documento DOM
-          const doc = parser.parseFromString(html, 'text/html');
-          // Procura o elemento pelo ID
-          const elemento = doc.getElementById('tituloSite');
+          const newHTML = parser.parseFromString(html, 'text/html');
           document.getElementById('conteudo').innerHTML = html;// Define o HTML da página dentro do elemento de conteúdo
-          document.getElementById('tituloSite').innerHTML = elemento.innerHTML;// Define o title da página que está sendo acessada
+          document.getElementById('tituloSite').innerText = newHTML.getElementsByTagName('title')[0].innerText;// Define o title da aba do navegador para ser o title da página que está sendo acessada
         }else{
             document.getElementById('conteudo').innerHTML = '<h2 id="titulo">Page not found</h2>'
         }
